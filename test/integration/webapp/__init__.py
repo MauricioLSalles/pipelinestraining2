719284c4df090dc2b1644cef229aa0fcda43d6ca
MauricioLsalles <mauricio.leano@jala.university>
2025-05-12 11:05:44 -0400

    test deploy dev

--- test/integration/webapp/__init__.py
+++ test/integration/webapp/__init__.py
@@ -4,4 +4,4 @@ import pytest
 @pytest.fixture(scope="session")
 def base_url():
     #return os.getenv("API_BASE_URL", "http://localhost:5000")    
-    return os.getenv("API_BASE_URL", "http://172.18.0.1:5000")
+    return os.getenv("API_BASE_URL", "http://localhost:5000")
